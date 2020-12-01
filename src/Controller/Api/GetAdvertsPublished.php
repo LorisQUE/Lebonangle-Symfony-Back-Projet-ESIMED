eed1cc856dfd2cddc06dc3c69c536140ff3dcb71
Maco13 <36083989+Maco13@users.noreply.github.com>
2020-12-01 19:41:32 +0100

    fix adverts publiées

--- src/Controller/Api/GetAdvertsPublished.php
+++ src/Controller/Api/GetAdvertsPublished.php
@@ -1,22 +1,28 @@
 <?php
 namespace App\Controller\Api;
 
-use App\Entity\Advert;
-use App\Entity\Picture;
 use App\Repository\AdvertRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 final class GetAdvertsPublished
 {
+
+    /**
+     * @var AdvertRepository
+     */
     private AdvertRepository $advertRepository;
+
     public function __construct(AdvertRepository $advertRepository)
     {
         $this->advertRepository = $advertRepository;
     }
+
     public function __invoke(Request $request)
     {
+        if ($request->get('category_id')){
+            return $this->advertRepository->findBy(["state"=>"published","category"=>$request->get('category_id')]);
+        }
         return $this->advertRepository->findBy(["state"=>"published"]);
     }
+
 }
